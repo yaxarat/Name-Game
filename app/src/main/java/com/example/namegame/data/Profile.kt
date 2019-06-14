4ad6a519df909ccbecb116e6ed0deacddc7792e0
Yashar <ya.atajan@willowtreeapps.com>
2019-06-13 22:02:49 -0400

    portion of profile.kt commented out due to json error

--- app/src/main/java/com/example/namegame/data/Profile.kt
+++ app/src/main/java/com/example/namegame/data/Profile.kt
@@ -5,8 +5,9 @@ data class Profile(
     var headshot: Headshot,
     var id: String,
     var jobTitle: String,
-    var lastName: String
-//    var slug: String,
-//    var socialLinks: List<Any>,
-//    var type: String
+    var lastName: String,
+    var slug: String,
+    // TODO: This part creates error with Moshi
+    //var socialLinks: List<Any>,
+    var type: String
 )
